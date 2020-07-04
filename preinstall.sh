4e49faca947471056e0f5bd1aa77646eff45a6d0
Luis Espinoza <luis.espinoza@lkmx.io>
2020-07-03 17:26:09 -0700

    Remove pre-install script and pre-install hook

--- preinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -n "$NPM_TOKEN" ]; then
-	echo -e "registry=https://npm.pkg.github.com/lkmx\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc;
-else
-	echo "registry=https://npm.pkg.github.com/lkmx" > .npmrc;
-fi
