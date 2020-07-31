24eecc3d226498844c24af2ff9649f8f0cb85f04
edith moreno <edith.moreno@lkmx.io>
2020-07-31 16:31:09 -0700

    Pages modified.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,134 @@
 <template>
   <default-layout>
-    <h1>Flare Starter</h1>
+    <base-page>
+      <!-- SimpleHero -->
+      <section class="intro">
+        <simple-hero>
+          <span>Revolucionamos</span>
+          <h1 class="title">Todo lo que necesitas</h1>
+          <p class="description">Ofrecemos la red de servicios más robusta y flexible</p>
+          <g-link to="/services" class="our-services">Nuestros servicios</g-link>
+        </simple-hero>
+      </section>
+
+      <!-- TripletColumns / Stats -->
+      <section class="stats">
+          <triplet-column>
+            <template v-slot:left>
+                <span class="first__number">10K</span>
+                <span class="first__text">Envíos</span>
+            </template>
+            <template v-slot:middle>
+                <span class="second__number">100K</span>
+                <span class="second__text">Toneladas</span>
+                <span class="second__text">enviadas</span>
+            </template>
+            <template v-slot:right>
+                <span class="third__text">Todo tipo de carga</span>
+                <div class="third__grid">
+                  <span>Seca</span>
+                  <span>Peligrosa</span>
+                  <span>Refrigerada</span>
+                  <span>Valiosa</span>
+                </div>
+            </template>
+          </triplet-column>
+      </section>
+
+      <!-- TripletColumns / Service Row 1 -->
+      <section class="service-row-1">
+          <triplet-column>
+            <template v-slot:left>
+                <h2 class="service__name">Marítimo FCL</h2>
+                <p class="service__description">Mayor rentabilidad por volumen, seguridad y flexibilidad en la entrega de tu mercancía de importación y exportación.</p>
+                <g-link to="/service/maritime-fcl" class="service__button">Leer Más</g-link>
+            </template>
+            <template v-slot:middle>
+                <g-image src="~/assets/images/home-grid-1.jpg" alt="Engagement"></g-image>
+            </template>
+            <template v-slot:right>
+                <h2 class="service__name">Marítimo LCL</h2>
+                <p class="service__description">Ideal para emprendedores y empresas que requieren el servicio marítimo sin la necesidad de enviar un contenedor completo.</p>
+                <g-link to="/" class="service__button">Leer Más</g-link>
+            </template>
+          </triplet-column>
+      </section>
+
+      <!-- TripletColumns / Service Row 2 -->
+      <section class="service-row-2">
+          <triplet-column>
+            <template v-slot:left>
+                <g-image src="~/assets/images/home-grid-2.jpg" alt="Reliability"></g-image>
+            </template>
+            <template v-slot:middle>
+                <h2 class="service__name">Terrestre</h2>
+                <p class="service__description">Total flexibilidad en rutas, equipo y tiempo de carga y descarga.</p>
+                <g-link to="/services" class="service__button">Leer Más</g-link>
+            </template>
+            <template v-slot:right>
+                <g-image src="~/assets/images/home-grid-3.jpg" alt="Trust"></g-image>
+            </template>
+          </triplet-column>
+      </section>
+
+      <!-- TripletColumns / Service Row 3 -->
+      <section class="service-row-3">
+          <triplet-column>
+            <template v-slot:left>
+                <h2 class="service__name">Ferroviario</h2>
+                <p class="service__description">Para envío de grandes volúmenes de cualquier tipo de mercancía a México y Estados Unidos.</p>
+                <g-link to="/" class="service__button">Leer Más</g-link>
+            </template>
+            <template v-slot:middle>
+                <g-image src="~/assets/images/home-grid-4.jpg" alt="Engagement"></g-image>
+            </template>
+            <template v-slot:right>
+                <h2 class="service__name">Aéreo</h2>
+                <p class="service__description">El mejor tiempo de entrega en envíos de carga seca y refrigerada a México y el mundo.</p>
+                <g-link to="/" class="service__button">Leer Más</g-link>
+            </template>
+          </triplet-column>
+      </section>
+      
+      <!--TextBanner / Contact Us-->
+      <section class="contact-us">
+        <text-banner>
+          <h1>CONTAMOS SOLUCIONES PARA EL MANEJO DE TU CARGA <span>EN REPOSO Y EN MOVIMIENTO</span></h1>
+          <g-link to="/" class="contact-btn">Contáctanos</g-link>
+        </text-banner>
+      </section>
+
+      <!-- TwinColumns / Warehousing -->
+      <section class="warehousing">
+        <twin-columns>
+          <template v-slot:left>
+            <h2 class="service__name">Almacenes</h2>
+            <p class="service__description">Mayor rentabilidad por volumen, seguridad y flexibilidad en la entrega de tu mercancía de importación y exportación. </p>
+            <g-link to="/" class="service__button">Leer Más</g-link>
+          </template>
+          <template v-slot:right>
+             <g-image src="~/assets/images/warehouse.jpg" alt="Engagement"></g-image>
+          </template>
+        </twin-columns>
+      </section>
+
+      <!-- TwinColumns / Patio -->
+      <section class="patio">
+        <twin-columns>
+          <template v-slot:left>
+            <g-image src="~/assets/images/patio.jpg" alt="Engagement"></g-image>
+          </template>
+          <template v-slot:right>
+            <h1 class="service__name">Patio</h1>
+            <p class="service__description">Mayor rentabilidad por volumen, seguridad y flexibilidad en la entrega de tu mercancía de importación y exportación.</p>
+            <g-link to="/" class="service__button">Leer Más</g-link>
+          </template>
+        </twin-columns>
+      </section>
+    </base-page>
   </default-layout>
 </template>
+
 
 <script>
 export default {
@@ -11,3 +137,417 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+body {
+  .intro {
+    .simple-hero {
+      background-image: url('../assets/images/hero-image.jpg');
+      background-size: cover;
+      &__content {
+        color: white;
+        padding-top: rem(245px);
+        span {
+          text-transform: uppercase;
+          letter-spacing: 2.4px;
+        }
+        .title {
+          color: white;
+          font-size: 60px;
+          font-weight: 900;
+          text-transform: uppercase;
+          text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.51);
+        }
+        .our-services {
+          @include button($brand-yellow, black);
+          width: rem(250px);
+          text-transform: uppercase;
+          text-align: center;
+          text-decoration: none;
+          font-weight: 900;
+        }
+        &>* {
+          position: unset;
+        }
+      }
+    }
+
+    .twin-columns .twins-column--start {
+      align-self: center;
+    }
+
+    .twins-section--start {
+      .title {
+        font-size: rem(50px);
+        font-weight: 900;
+        line-height: 1.3;
+
+        span {
+          display: block;
+        }
+      }
+
+      .description {
+        margin-bottom: $gutter-large;
+      }
+
+      .delegate {
+        //@include button($brand-primary, $brand-white);
+      }
+    }
+  }
+  .stats {
+    background-color: $brand-blue-6;
+    .triplets {
+      padding: rem(28px) 0;
+      &-section {
+        &--start, &--middle, &--end {
+          .triplets {
+            &-column {
+              align-self: center;
+              &--start, &--middle, &--end {
+                span {
+                  display: block;
+                  color: white;
+                  text-transform: uppercase;
+                  font-size: rem(25px);
+                  font-weight: 900;
+                  vertical-align: middle;
+                  &.first {
+                    &__number {
+                      font-size: rem(60px);
+                    }
+                    &__text {
+                      font-size: rem(28px);
+                    }
+                  }
+                  &.second {
+                    &__number {
+                      font-size: rem(68px);
+                      text-align: center;
+                    }
+                    &__text {
+                      text-align: center;
+                      font-size: rem(25px);
+                      &:last-child {
+                        font-size: rem(30px);
+                      }
+                    }
+                  }
+                  &.third {
+                    &__text {
+                      margin-bottom: rem(16px);
+                      font-size: rem(23px);
+                      display: inline-block;
+                      width: auto;
+                      padding: 0 rem(16px);
+                    }
+                  }
+                }
+                .third {
+                  &__grid {
+                    display: grid;
+                    grid-template-columns: repeat(2, 1fr);
+                    grid-template-rows: repeat(2, 1fr);
+                    grid-row-gap: rem(8px);
+                    grid-column-gap: rem(8px);
+                    padding: 0 rem(5px);
+                    span {
+                      font-size: rem(16px);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .service-row-1, .service-row-2, .service-row-3 {
+    .triplets {
+      &-section {
+        &--start, &--middle, &--end {
+          .triplets {
+            &-column {
+              &--start, &--middle, &--end {
+                .service {
+                  &__name {
+                    color: white;
+                    text-transform: uppercase;
+                    font-weight: 900;
+                    border-bottom: 3px solid $brand-orange;
+                    padding-bottom: rem(16px);
+                    display: inline-block;
+                  }
+                  &__description {
+                    color: white;
+                    @include respond-to("large and up") {
+                      width: 85%;
+                    }
+                  }
+                  &__button {
+                    @include button($brand-yellow, black);
+                    width: auto;
+                    padding: rem(12px);
+                    text-transform: uppercase;
+                    text-align: center;
+                    text-decoration: none;
+                    font-weight: 900;
+                  }
+                }
+                img {
+                  width: 100%;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .service-row-1, .service-row-3 {
+    background-color: $brand-blue-5;
+    .triplets {
+      &-section {
+        &--start {
+          .triplets {
+            &-column {
+              &--start {
+                padding: rem(24px);
+              }
+            }
+          }
+        }
+        &--middle {
+          .triplets {
+            &-column {
+              &--middle {
+                padding: 0;
+              }
+            }
+          }
+        }
+        &--end {
+          .triplets {
+            &-column {
+              &--end {
+                padding: rem(24px);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .service-row-1 {
+    background-color: $brand-blue-5;
+    .triplets {
+      &-section {
+        &--start {
+          .triplets {
+            &-column {
+              &--start {
+                background-color: $brand-blue-4;
+              }
+            }
+          }
+        }
+        &--end {
+          .triplets {
+            &-column {
+              &--end {
+                background-color: $brand-blue-7;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .service-row-2 {
+    background-color: $brand-blue-7;
+    .triplets {
+      &-section {
+        &--start, &--end {
+          .triplets {
+            &-column {
+              &--start, &--end {
+                padding: 0;
+                img {
+                  width: 100%;
+                }
+              }
+            }
+          }
+        }
+        &--middle {
+          .triplets {
+            &-column {
+              &--middle {
+                padding: rem(24px);
+                background-color: $brand-blue-3;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .service-row-3 {
+    .triplets {
+      &-section {
+        &--start {
+          .triplets {
+            &-column {
+              &--start {
+                background-color: $brand-blue-2;
+              }
+            }
+          }
+        }
+        &--end {
+          .triplets {
+            &-column {
+              &--end {
+                background-color: $brand-blue-3;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .contact-us {
+    background-color: $brand-blue-6;
+    .text-banner {
+      padding: rem(124px) 0;
+      h1 {
+        color: white;
+        font-weight: 900;
+        margin-bottom: rem(30px);
+        span {
+          display: block;
+          margin-top: rem(24px);
+          font-size: rem(40px);
+        }
+      }
+      .contact-btn {
+        @include button($brand-yellow, black);
+        width: auto;
+        margin: 0 auto;
+        padding: rem(12px);
+        text-transform: uppercase;
+        text-align: center;
+        text-decoration: none;
+        font-weight: 900;
+      }
+    }
+  }
+  .warehousing, .patio {
+    .twin-columns {
+      height: rem(336px);
+      .twins {
+        &-section {
+          &--start, &--end {
+            .twins {
+              &-column {
+                &--start, &--end {
+                  .service {
+                    &__name {
+                      color: white;
+                      text-transform: uppercase;
+                      font-weight: 900;
+                      border-bottom: 3px solid $brand-orange;
+                      padding-bottom: rem(16px);
+                      display: inline-block;
+                    }
+                    &__description {
+                      color: white;
+                      @include respond-to("large and up") {
+                        width: 85%;
+                      }
+                    }
+                    &__button {
+                      @include button($brand-yellow, black);
+                      width: auto;
+                      padding: rem(12px);
+                      text-transform: uppercase;
+                      text-align: center;
+                      text-decoration: none;
+                      font-weight: 900;
+                    }
+                  }
+                  img {
+                    height: rem(336px);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .warehousing {
+    background-color: $brand-blue-4;
+    .twin-columns {
+      .twins {
+        &-section {
+          &--start {
+            .twins {
+              &-column {
+                &--start {
+                  width: rem(336px);
+                  justify-self: center;
+                  padding: rem(24px);
+                }
+              }
+            }
+          }
+          &--end {
+            .twins {
+              &-column {
+                &--end {
+                  width: 100%;
+                  padding: 0;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .patio {
+    background-color: $brand-blue-6;
+    .twin-columns {
+      .twins {
+        &-section {
+          &--start {
+            .twins {
+              &-column {
+                &--start {
+                  width: 100%;
+                  padding: 0;
+                }
+              }
+            }
+          }
+          &--end {
+            .twins {
+              &-column {
+                &--end {
+                  width: rem(336px);
+                  justify-self: left;
+                  padding: rem(24px);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
